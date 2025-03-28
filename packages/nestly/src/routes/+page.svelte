665eaa18133c3a2af891d42b32ecf0a1c6ce33f7
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-03-28 18:52:35 -0500

    Proper whatever.

--- packages/nestly/src/routes/+page.svelte
+++ packages/nestly/src/routes/+page.svelte
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="/styles/owo.css" />
 <h1 class="amber">Svelte</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <a href="https://google.com" target="_blank" rel="noopener noreferrer">google.com</a>
