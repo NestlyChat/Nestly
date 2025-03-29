425eba30a7404818dc5baf8f08a8d878dd3a42d1
cat <73869003+caffeinedubz@users.noreply.github.com>
2025-03-29 16:10:08 -0500

    i did a funny

--- packages/nestly/src/routes/+page.svelte
+++ packages/nestly/src/routes/+page.svelte
@@ -1,3 +1,32 @@
-<h1 class="amber">Svelte</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<a href="https://google.com" target="_blank" rel="noopener noreferrer">google.com</a>
+<body class="app">
+        <div class="appBase">
+
+        <ul class="serverNav">
+            <div class="nestHome"></div>
+            <div class="nestSpace"></div>
+        </ul>
+        <div class="appHeader">
+            <div class="nestTitle">
+            <div class="nestSpaceDescription"></div>
+            </div>
+            <div class="primaryUser">
+                <div class="nestUserAvatar"></div>
+                <div class="nestUserName"></div>
+            </div>
+        </div>
+
+        <div class="chatContent">
+            <div class="channelList"></div>
+            <div class="nestChatHistory"></div>
+        <div class="chatInput">
+            <form class="chatForm">
+                <input type="text" class="chatInputField" placeholder="Type a message..." />
+                <button type="submit" class="chatSendButton">Send</button>
+        </div>
+
+        <div class="nestMemberList"></div>
+        </div>
+
+    </div>
+</body>
+<!-- thanks catppuccin for the color scheme -->
