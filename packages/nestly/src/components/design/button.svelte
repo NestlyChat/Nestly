3c541a33333a2d1367f826ddfcaf3bd32566dd37
Riddim_GLiTCH <riddim_glitch@proton.me>
2025-03-30 23:43:47 +0200

    Fix the buttons lol

--- packages/nestly/src/components/design/button.svelte
+++ packages/nestly/src/components/design/button.svelte
@@ -20,3 +20,16 @@
     {/if}
     {text}
 </button>
+
+<button class="menuButton" aria-label={text}>
+    {#if dPath}
+        <svg
+            viewBox={viewBox || "0 0 24 24"}
+            width={size || "24"}
+            height={size || "24"}
+        >
+            <path fill={color || "currentColor"} d={dPath} />
+        </svg>
+    {/if}
+    {text}
+</button>
