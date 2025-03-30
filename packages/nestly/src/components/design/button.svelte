6a9d7871e1722372e65c4dfe71cc12ebf5f91cf2
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-03-30 16:15:49 -0500

    test

--- packages/nestly/src/components/design/button.svelte
+++ packages/nestly/src/components/design/button.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
     let { text, viewBox, size, color, dPath } = $props<{
-        text: string,
-        viewBox?: string,
-        size?: string,
-        color?: string,
-        dPath?: string 
+        text: string;
+        viewBox?: string;
+        size?: string;
+        color?: string;
+        dPath?: string;
     }>();
 </script>
 
-<div class="flex">
-    <button class="flex chatSendButton" aria-label={text}>
-        {#if dPath}
-            <svg viewBox={viewBox || "0 0 24 24"} width={size || "24"} height={size || "24"}>
-                <path fill={color || "currentColor"} d={dPath}/>
-            </svg>
-        {/if}
-        {text}
-    </button>
-</div>
+<button class="chatSendButton flex" aria-label={text}>
+    {#if dPath}
+        <svg
+            viewBox={viewBox || "0 0 24 24"}
+            width={size || "24"}
+            height={size || "24"}
+        >
+            <path fill={color || "currentColor"} d={dPath} />
+        </svg>
+    {/if}
+    {text}
+</button>
