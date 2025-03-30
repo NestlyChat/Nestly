9e532bc10ce720406b9d912b566bad51a11972da
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-03-30 15:07:21 -0500

    design template

---
+++ packages/nestly/src/components/design/button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let {text, viewBox, width, height, color, dPath} = $props<{text: string, viewBox?: string, width?: number, height?: number, color?: string, dPath?: string}>();
+</script>
+
+<div class="flex">
+    <svg {viewBox} {width} {height} style="color: {color}">
+        <path fill={color} d={dPath}/>
+    </svg>
+    <button class="chatSendButton" aria-label={text}>
+        {text}
+    </button>
+</div>
--- packages/nestly/src/components/textbar.svelte
+++ packages/nestly/src/components/textbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    let text = $state("")
+    let text = $state<{text: string}>({text: 'hello'})
+    let {fuckya} = $props<{fuckya: string}>()
 </script>
 
-<input type="text" bind:value={text} class="chatInputField" placeholder="Type a message..." />
+<input type="text" bind:value={text.text} class="chatInputField" placeholder="Type a message..." />
