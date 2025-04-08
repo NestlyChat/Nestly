8495631985414486290095823d96c9707a204a4a
Robert Delaney <Doggybootsy@gmail.com>
2025-04-07 19:01:34 -0700

    Fix hostname

--- packages/nestly/dev.ts
+++ packages/nestly/dev.ts
@@ -1,4 +1,25 @@
 import { patchSveltekit, startDevServer } from "@eslym/sveltekit-adapter-bun";
 
 await patchSveltekit();
-await startDevServer();
+
+const port = Number(process.env.PORT || 5173);
+
+console.log = new Proxy(console.log, {
+	apply(target, thisArg, argArray) {
+		if (argArray.length === 1 && argArray[0] === `Serving on http://0.0.0.0:${port}/`) {
+			target.apply(thisArg, [
+				`Serving on http://localhost:${port}/`
+			]);
+
+			console.log = target;
+			return 
+		}
+
+		return target.apply(thisArg, argArray);
+	},
+})
+
+await startDevServer({
+	host: "0.0.0.0",
+	port
+});
